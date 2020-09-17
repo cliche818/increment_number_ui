ed6bef298019e89e2c680d9d9031b202dd7de002
Jeff Li <jeff.chun.yinli@gmail.com>
2020-09-17 16:02:55 -0400

    create button for current number on number page

--- src/pages/NumberPage.js
+++ src/pages/NumberPage.js
@@ -3,11 +3,55 @@ import requireAuthentication from '../AuthenticatedComponent';
 import LogoutButton from '../components/LogoutButton'
 
 class NumberPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayMessage: 'No action taken yet'
+    }
+  }
+
+  apiToken = () => {
+    return sessionStorage.getItem('api_token')
+  }
+
+  createDisplayMessage(action, json) {
+    if (!!json["data"]) {
+      const number = json["data"]["attributes"]["number"]
+      return `Action: ${action}, number: ${number}`
+    } else if (!!json["error"]) {
+      return `Action: ${action}, error: ${json["error"]["title"]} | ${json["error"]["title"]}`
+    } else {
+      return `Action: ${action}, unknown error`
+    }
+  }
+
+  currentNumber = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/v1/current`,
+      {
+        headers: {
+          "Authorization": `Bearer ${this.apiToken()}`
+        },
+        method: "GET",
+      }
+    )
+    const json = await response.json()
+    
+    const displayMessage = this.createDisplayMessage('current', json)
+
+    this.setState({displayMessage: displayMessage})
+  }
+
   render() {
     return (
       <div>
         <h1>Number Page</h1>
-        <LogoutButton history={this.props.history}/>
+        <LogoutButton history={this.props.history} />
+
+        <br />
+
+        <p>{`${this.state.displayMessage}`}</p>
+
+        <button onClick={this.currentNumber}>Current Number </button>
       </div>
     )
   }
